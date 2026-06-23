3efda8e64f67fa5f61410475b26424df288477cc
nhiy3nbf <peterthang0314@gmail.com>
2026-06-24 04:33:28 +0700

    Delete AppUni directory

--- AppUni/app/routes/dashboard.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-dashboard = Blueprint('dashboard', __name__)
-
-@dashboard.route('/dashboard')
-def home():
-    return '<h1>Hello this is dashboard<h1>'

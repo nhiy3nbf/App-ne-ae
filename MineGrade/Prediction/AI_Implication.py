5a900c329a9a0a6487c352609311a48a6f04eb3a
TrungNGM <samonix788@gmail.com>
2026-07-25 23:02:57 +0700

    Update AI_Implication.py

--- MineGrade/Prediction/AI_Implication.py
+++ MineGrade/Prediction/AI_Implication.py
@@ -62,7 +62,7 @@ def get_giga_steve_response(user_text, chat_history=None):
             config=types.GenerateContentConfig(
                 system_instruction=GIGA_STEVE_SYSTEM_INSTRUCTION,
                 temperature=0.7,
-                max_output_tokens=450,
+                max_output_tokens=600,
             )
         )
         return response.text.strip()

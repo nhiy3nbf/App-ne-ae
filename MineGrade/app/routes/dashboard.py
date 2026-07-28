1fb7919363e38b8806720972c31a2ed63b8b239b
nhiy3nbf <peterthang0314@gmail.com>
2026-07-28 21:27:32 +0700

    Delete MineGrade directory

--- MineGrade/app/routes/dashboard.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-dashboard = Blueprint('dashboard', __name__)
-
-@dashboard.route('/dashboard')
-def home():
-    return render_template('dashboard.html')
--- MineGrade/app/routes/profile.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-profile = Blueprint('profile', __name__)
-
-@profile.route('/profile')
-def account():
-    return render_template('profile.html')
